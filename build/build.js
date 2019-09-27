634f3b10de2d2d27c93c973e46d396d2264dff9d
Chuck Masucci <cmasucci@gmail.com>
2019-09-27 11:12:22 -0700

    New build script

---
+++ build/build.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+const fs = require('fs');
+const sass = require('node-sass');
+
+const args = process.argv.slice(2, process.argv.length);
+const scssPath = 'src/scss/';
+const regex = /^_(.*).scss$/;
+const defaultImports = [
+  'functions',
+  'variables',
+  'mixins/mixins',
+  'rfs/rfs',
+  'utilities/utilities',
+];
+
+const build = () => {
+  const importFiles = getSassImports(args);
+  const result = sass.renderSync({
+    data: importFiles,
+    includePaths: [scssPath],
+    outputStyle: 'compressed',
+    outFile: `dist/${filename}.css`,
+    sourceMap: `dist/${filename}.css.map`,
+  });
+  const filename = args.length > 0 ? args.join('-') : 'forgerock';
+  fs.writeFileSync(`dist/${filename}.css`, result.css, err => {
+    if (err) {
+      console.error('CSS build error: ', err);
+    }
+  });
+  fs.writeFileSync(`dist/${filename}.css.map`, result.map, err => {
+    if (err) {
+      console.error('Source Map build error: ', err);
+    }
+  });
+  console.info('CSS build complete');
+};
+
+const getSassImports = components => {
+  const list =
+    components.length > 0 ? formatImports(components) : getAllFiles();
+  const includes = formatImports(defaultImports);
+  return includes.join('\n') + '\n' + list.join('\n');
+};
+
+const getAllFiles = () => {
+  const files = fs.readdirSync(scssPath);
+  const filtered = files.filter(item => {
+    const cleaned = item.match(regex);
+    if (!cleaned) {
+      return false;
+    }
+    return !defaultImports.includes(cleaned[1]);
+  });
+  const list = filtered.map(item => {
+    const cleaned = item.match(regex);
+    return `@import '${cleaned[1]}';`;
+  });
+  return list;
+};
+
+const formatImports = items => {
+  return items.map(item => `@import '${item}';`);
+};
+
+build();
