b9a87b410590a3b9199b510f1912d1e941d93e2c
Chuck Masucci <cmasucci@gmail.com>
2019-10-02 12:56:01 -0700

    Cleanup and slight structure updates

--- build/build.js
+++ build/build.js
@@ -2,9 +2,12 @@
 const fs = require('fs');
 const sass = require('node-sass');
 
-const args = process.argv.slice(2, process.argv.length);
-const scssPath = 'src/scss/';
-const regex = /^_(.*).scss$/;
+const config = {
+  dist: 'dist/',
+  lib: 'lib/',
+  scss: 'src/scss/',
+};
+
 const defaultImports = [
   'functions',
   'variables',
@@ -13,54 +16,57 @@ const defaultImports = [
   'utilities/utilities',
 ];
 
-const build = args => {
+const args = process.argv.slice(2, process.argv.length);
+const regex = /^_(.*).scss$/;
+
+const build = (args) => {
   const importFiles = getSassImports(args);
-  const filename = args.length > 0 ? args.join('-') : 'forgerock';
+  const filename =
+    args.length > 0 ? `${config.dist}${args.join('-')}.css` : `${config.lib}forgerock.css`;
   const result = sass.renderSync({
     data: importFiles,
-    includePaths: [scssPath],
+    includePaths: [config.scss],
     outputStyle: 'compressed',
-    outFile: `dist/${filename}.css`,
-    sourceMap: `dist/${filename}.css.map`,
+    outFile: filename,
+    sourceMap: `${filename}.map`,
   });
-  fs.writeFileSync(`dist/${filename}.css`, result.css, err => {
+  fs.writeFileSync(filename, result.css, (err) => {
     if (err) {
       console.error('CSS build error: ', err);
     }
   });
-  fs.writeFileSync(`dist/${filename}.css.map`, result.map, err => {
+  fs.writeFileSync(`${filename}.map`, result.map, (err) => {
     if (err) {
       console.error('Source Map build error: ', err);
     }
   });
-  console.info('CSS build complete');
+  console.info(`Built ${filename}`);
 };
 
-const getSassImports = components => {
-  const list =
-    components.length > 0 ? formatImports(components) : getAllFiles();
+const getSassImports = (components) => {
+  const list = components.length > 0 ? formatImports(components) : getAllFiles();
   const includes = formatImports(defaultImports);
   return includes.join('\n') + '\n' + list.join('\n');
 };
 
 const getAllFiles = () => {
-  const files = fs.readdirSync(scssPath);
-  const filtered = files.filter(item => {
+  const files = fs.readdirSync(config.scss);
+  const filtered = files.filter((item) => {
     const cleaned = item.match(regex);
     if (!cleaned) {
       return false;
     }
     return !defaultImports.includes(cleaned[1]);
   });
-  const list = filtered.map(item => {
+  const list = filtered.map((item) => {
     const cleaned = item.match(regex);
     return `@import '${cleaned[1]}';`;
   });
   return list;
 };
 
-const formatImports = items => {
-  return items.map(item => `@import '${item}';`);
+const formatImports = (items) => {
+  return items.map((item) => `@import '${item}';`);
 };
 
 build(args);
